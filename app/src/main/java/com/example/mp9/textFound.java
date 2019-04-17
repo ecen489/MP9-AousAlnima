f820ec04855d556f02d10f995ed12cdbebd38a43
Aous <aous@Anass-MacBook-Air.local>
2019-04-17 09:28:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mp9/textFound.java
@@ -0,0 +1,16 @@
+package com.example.mp9;
+
+import android.support.annotation.Nullable;
+
+public class textFound {
+    String tex;
+
+    public textFound(String textf) {
+        tex = textf;
+    }
+
+    @Nullable
+    public String getTex() {
+        return tex;
+    }
+}
